f63f34ec6326344355c980b0c4754125f57bcdea
Lawrence Chang <lawrencechangbc@gmail.com>
2016-03-28 10:40:55 -0700

    Initial commit.

--- farmersflickr2/ViewController.swift
+++ farmersflickr2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Second attempt at this app, by Lawrence.")
     }
 
     override func didReceiveMemoryWarning() {
